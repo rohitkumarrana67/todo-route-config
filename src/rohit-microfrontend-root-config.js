ebf29f8eefb47c1421858ab12b6e6d36c387d81e
Rohit Kumar Rana <rohitkumarrana67@gmail.com>
2020-12-26 13:27:04 +0530

    first commit

---
+++ src/rohit-microfrontend-root-config.js
@@ -0,0 +1,45 @@
+// import { registerApplication, start } from "single-spa";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+// registerApplication({
+//   name: "@rohit-microfrontend/navbar",
+//   app: () => System.import("@rohit-microfrontend/navbar"),
+//   activeWhen: ["/"]
+// });
+
+// registerApplication({
+//   name: "@rohit-microfrontend/footer",
+//   app: () => System.import("@rohit-microfrontend/footer"),
+//   activeWhen: ["/footer"]
+// });
+
+// start({
+//   urlRerouteOnly: true,
+// });
+
+
+import { registerApplication, start } from 'single-spa';
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from 'single-spa-layout';
+const routes = constructRoutes(document.querySelector('#single-spa-layout'));
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+applications.forEach(registerApplication);
+start();
+
